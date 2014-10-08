5964c43ee6d626863165660c49df386266ffe5d2
Emerson Veenstra <emerson@emersonveenstra.net>
2014-10-07 20:04:47 -0400

    Fixed require erro

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var scheduler = require('node-schedule');
+var shows = require('./lib/shows');
 
 
 // all environments
